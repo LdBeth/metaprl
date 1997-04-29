b2128fc8e1a651cfecf28b5b42453dfceb911895
jyh <jyh@5109f7fa-8300-0410-942d-a7a480986ffa>
1997-04-29 16:16:47 +0000

    Modified ml_format.

---
+++ refiner/ml_format.mli
@@ -0,0 +1,20 @@
+(*
+ * Convert terms to ML expressions.
+ *)
+
+open Ml_format_sig
+
+module FormatTerm : FormatTermSig
+
+(*
+ * $Log$
+ * Revision 1.1  1997/04/29 16:16:46  jyh
+ * Modified ml_format.
+ *
+ *
+ * -*-
+ * Local Variables:
+ * Caml-master: "refiner"
+ * End:
+ * -*-
+ *)
