0d1c116f7eeefdc324df963c0871a536d8c7b8bb
nogin <nogin@5109f7fa-8300-0410-942d-a7a480986ffa>
1997-08-16 00:18:59 +0000

    * Added several functions (used in evaluator)  * Fixed mk_dep0_dep2_dep0_dep2_term

--- refiner/term.mli
+++ refiner/term.mli
@@ -355,27 +355,43 @@ val mk_xlambda_term : string -> term -> term
 (*
  * Compute the "shape" of the term that can be used for reductions.
  * Terms are reduced to these templates for indexing
- * purposes.  Each template just contains information
- * about the opname, the order and types of params,
- * and the arties of the subterms.
+ * purposes.  
  *)
-type shape =
-   { shape_opname : opname;
-     shape_params : shape_param list;
-     shape_arities : int list
-   }
-
-and shape_param =
-   ShapeNumber
- | ShapeString
- | ShapeToken
- | ShapeLevel
- | ShapeVar
+type shape 
 
 val shape_of_term : term -> shape
+val print_shape : shape -> unit
+
+(*
+ * ``Special'' terms to be used in reduction rules
+ *
+ * "canon_var" plays the same role as "var" in reduction rules but 
+ * the correspondig subterm should be evaluated before the reduction
+ * ("call by value" instead of "call by name")
+ *
+ * subst (v1,v2,v3,...,vm.T;t1;t2;t3;...;tn)
+ * it is an error if m!=n
+ * if n=m then subst(...) is T with v1 substituted to t2, v2 - to t2, etc.
+ *
+ *)
+
+val is_canon_var_term : term -> bool
+val dest_canon_var : term -> string
+val mk_canon_var_term : string -> term
+
+val is_subst_term : term -> bool
+val dest_subst : term -> term * (string list * term list)
+val mk_subst_term : term -> (string * term) list -> term
+val make_subst_term : term -> string list -> term list -> term
+val make_1subst_term : term -> string -> term -> term
+val make_2subst_term : term -> string -> string -> term -> term -> term
 
 (*
  * $Log$
+ * Revision 1.5  1997/08/16 00:18:59  nogin
+ *  * Added several functions (used in evaluator)
+ *  * Fixed mk_dep0_dep2_dep0_dep2_term
+ *
  * Revision 1.4  1997/08/07 19:43:50  jyh
  * Updated and added Lori's term modifications.
  * Need to update all pattern matchings.
