bf740648efe110b8cd140448161b904f5d4c7645
jyh <jyh@5109f7fa-8300-0410-942d-a7a480986ffa>
1998-03-03 04:06:02 +0000

    Added filter bin.

---
+++ filter/filter_bin.ml
@@ -0,0 +1,153 @@
+(*
+ * Process a binary file.
+ * This file is taken from a previously parsed file,
+ * or from the library.
+ *)
+
+open Arg
+open Printf
+
+open Debug
+
+open Filter_summary
+open Filter_summary_type
+open Filter_cache
+open Filter_prog
+
+(*
+ * Include directories.
+ *)
+let include_path = ref ["."]
+   
+let add_include dir =
+   include_path := !include_path @ [dir]
+
+(*
+ * Need some info about types and extraction.
+ *)
+module type CompileInfoSig =
+sig
+   type proof
+   type expr
+   type ctyp
+   type item
+
+   val extract : (proof, ctyp, expr, item) module_info -> string -> (item * (int * int)) list
+   val compile : (item * (int * int)) list -> unit
+end
+
+(*
+ * Compile from a pre-parsed file.
+ *)
+module MakeCompile (**)
+   (Info : CompileInfoSig)
+   (FilterCache : SummaryCacheSig
+    with type sig_ctyp  = Info.ctyp
+    with type str_proof = Info.proof
+    with type str_expr  = Info.expr
+    with type str_ctyp  = Info.ctyp
+    with type str_item  = Info.item
+    with type select    = select_type) =
+struct
+   let compile name =
+      let inline_hook cache (path, info) () =
+         ()
+      in
+      let kind, path =
+         if Filename.check_suffix name ".cmiz" then
+            InterfaceType, Filename.chop_suffix name ".cmiz"
+         else if Filename.check_suffix name ".cmit" then
+            InterfaceType, Filename.chop_suffix name ".cmit"
+         else if Filename.check_suffix name ".cmoz" then
+            ImplementationType, Filename.chop_suffix name ".cmoz"
+         else if Filename.check_suffix name ".cmot" then
+            ImplementationType, Filename.chop_suffix name ".cmot"
+         else
+            raise (Failure "Filter_parse.compile: invalid suffix")
+      in
+      let cache = FilterCache.create !include_path in
+      let info, _ = FilterCache.load cache path kind InterfaceType inline_hook () in
+      let items = Info.extract (FilterCache.info info) path in
+         Info.compile items
+end
+
+(*
+ * The two caches.
+ *)
+module SigCompileInfo =
+struct
+   type proof = unit
+   type expr  = MLast.expr
+   type ctyp  = MLast.ctyp
+   type item  = MLast.sig_item
+
+   let extract = extract_sig
+   let compile items =
+      (!Pcaml.print_interf) items
+end
+
+module StrCompileInfo =
+struct
+   type proof = proof_type
+   type expr  = MLast.expr
+   type ctyp  = MLast.ctyp
+   type item  = MLast.str_item
+
+   let extract = extract_str
+   let compile items =
+      (!Pcaml.print_implem) items;
+      eprintf "Writing output file%t" eflush;
+      flush stdout
+end
+
+module SigCompile = MakeCompile (SigCompileInfo) (SigFilterCache)
+module StrCompile = MakeCompile (StrCompileInfo) (StrFilterCache)
+                    
+(*
+ * Compile a file.
+ *)
+let process_file file =
+   if Filename.check_suffix file ".cmiz" or
+      Filename.check_suffix file ".cmit"
+   then
+      (* An interface file *)
+      SigCompile.compile file
+   else if Filename.check_suffix file ".cmoz" or
+           Filename.check_suffix file ".cmot"
+   then
+      (* An implementation file *)
+      StrCompile.compile file
+   else
+      raise (Bad "Filter_bin.main: file has a bogus suffix")
+
+(*
+ * Argument specification.
+ *)
+let spec =
+   ["-I", String add_include, "add an directory to the path for include files";
+    "-intf", String process_file, "compile an interface file";
+    "-impl", String process_file, "compile an implementation file"]
+
+(*
+ * Everything standard is taken care of.
+ * There should be a single file argument, so
+ * process it.
+ *)
+let main () =
+   Pcaml.input_file := "-";
+   Arg.parse spec process_file "Compile a Nuprl-Light binary file"
+
+let _ = Printexc.catch main ()
+
+(*
+ * $Log$
+ * Revision 1.1  1998/03/03 04:05:59  jyh
+ * Added filter bin.
+ *
+ *
+ * -*-
+ * Local Variables:
+ * Caml-master: "refiner"
+ * End:
+ * -*-
+ *)
---
+++ filter/filter_bin.mli
@@ -0,0 +1,16 @@
+(*
+ * Compile a binary file.
+ *)
+
+(*
+ * $Log$
+ * Revision 1.1  1998/03/03 04:06:02  jyh
+ * Added filter bin.
+ *
+ *
+ * -*-
+ * Local Variables:
+ * Caml-master: "refiner"
+ * End:
+ * -*-
+ *)
