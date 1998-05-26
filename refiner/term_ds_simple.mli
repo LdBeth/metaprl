9fbd4edfc5c0861fea9ceedca781f1cb992ee9be
nogin <nogin@5109f7fa-8300-0410-942d-a7a480986ffa>
1998-05-26 00:32:17 +0000

    Added old evaluator functions

--- refiner/term_ds_simple.mli
+++ refiner/term_ds_simple.mli
@@ -177,3 +177,28 @@ val is_free_seq_var : int -> string -> term -> bool
 val concl_addr : term -> int * int
 val replace_concl : term -> term -> term
 val replace_goal : term -> term -> term          (* One subgoal *)
+
+(*
+ * ``Special'' terms to be used in reduction rules
+ *
+ * "canon_var" plays the same role as "var" in reduction rules but 
+ * the correspondig subterm should be evaluated before the reduction
+ * ("call by value" instead of "call by name")
+ *
+ * subst (v1,v2,v3,...,vm.T;t1;t2;t3;...;tn)
+ * it is an error if m!=n
+ * if n=m then subst(...) is T with v1 substituted to t2, v2 - to t2, etc.
+ *
+ *)
+
+val is_canon_var_term : term -> bool
+val dest_canon_var : term -> string
+val mk_canon_var_term : string -> term
+
+val is_subst_term : term -> bool
+val dest_subst : term -> term * (string list * term list)
+val mk_subst_term : term -> (string * term) list -> term
+val make_subst_term : term -> string list -> term list -> term
+val make_1subst_term : term -> string -> term -> term
+val make_2subst_term : term -> string -> string -> term -> term -> term
+
