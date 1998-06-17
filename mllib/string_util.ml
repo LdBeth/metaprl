dd3d1a8310e22a8d5d5cc5ccddf781cd273b9d5d
jyh <jyh@5109f7fa-8300-0410-942d-a7a480986ffa>
1998-06-17 15:46:02 +0000

    Optimizing compiler.

--- mllib/string_util.ml
+++ mllib/string_util.ml
@@ -11,55 +11,73 @@ let _ =
    if !debug_load then
       eprintf "Loading String_util%t" eflush
 
+let debug_string =
+   create_debug (**)
+      { debug_name = "string";
+        debug_description = "check string bounds";
+        debug_value = false
+      }
+
 (*
  * Catch errors.
  *)
 let create name i =
-   if i < 0 or i > 65536 then
-      begin
-         eprintf "String_util.create: %s: %d < 0%t" name i eflush;
-         raise (Failure "String_util.create")
-      end;
+   if !debug_string then
+      if i < 0  then
+         begin
+            eprintf "String_util.create: %s: %d < 0%t" name i eflush;
+            raise (Failure "String_util.create")
+         end;
    String.create i
 
 let make name i c =
-   if i < 0 then
-      begin
-         eprintf "String_util.make: %s: %d < 0%t" name i eflush;
-         raise (Failure "String_util.make")
-      end;
+   if !debug_string then
+      if i < 0 then
+         begin
+            eprintf "String_util.make: %s: %d < 0%t" name i eflush;
+            raise (Failure "String_util.make")
+         end;
    String.make i c
 
 let sub name s i len =
-   let len' = String.length s in
-      if i >= 0 & len >= 0 & i + len < len' then
-         String.sub s i len
-      else
-         begin
-            eprintf "String_util.sub error: %s: %s.[%d]%t" name s i eflush;
-            raise (Failure "String_util.sub")
-         end
+   if !debug_string then
+      let len' = String.length s in
+         if i >= 0 & len >= 0 & i + len < len' then
+            String.sub s i len
+         else
+            begin
+               eprintf "String_util.sub error: %s: %s.[%d]%t" name s i eflush;
+               raise (Failure "String_util.sub")
+            end
+   else
+      String.sub s i len
 
 let blit name froms i tos j len =
-   let from_len = String.length froms in
-   let to_len = String.length tos in
-      if i >= 0 & j >= 0 & len >= 0 & i + len < from_len & j + len < to_len then
-         String.blit froms i tos j len
-      else
-         begin
-            eprintf "String_util.blit_error: %s: %s %d %s %d %d%t" name froms i tos j len eflush;
-            raise (Failure "String_util.blit")
-         end
+   if !debug_string then
+      let from_len = String.length froms in
+      let to_len = String.length tos in
+         if i >= 0 & j >= 0 & len >= 0 & i + len < from_len & j + len < to_len then
+            String.blit froms i tos j len
+         else
+            begin
+               eprintf "String_util.blit_error: %s: %s %d %s %d %d%t" name froms i tos j len eflush;
+               raise (Failure "String_util.blit")
+            end
+   else
+      String.blit froms i tos j len
 
 let set name s i c =
-   let len = String.length s in
-      if i >= 0 & i < len then
-         String.set s i c
-      else
-         begin
-            eprintf "String_util.set error: %s: %s.[%d] <- %c%t" name s i c eflush;
-            raise (Failure "String_util.set")
-         end
+   if !debug_string then
+      let len = String.length s in
+         if i >= 0 & i < len then
+            String.set s i c
+         else
+            begin
+               eprintf "String_util.set error: %s: %s.[%d] <- %c%t" name s i c eflush;
+               raise (Failure "String_util.set")
+            end
+   else
+      String.set s i c
 
 let get name s i =
    let len = String.length s in
@@ -70,8 +88,6 @@ let get name s i =
             eprintf "String_util.get error: %s: %s[%d]%t" name s i eflush;
             raise (Failure "String_util.get")
          end
-
-let rindex = String.rindex
 
 (*
  * Find a char in a string.
@@ -189,6 +205,9 @@ let rec concat s = function
 
 (*
  * $Log$
+ * Revision 1.8  1998/06/17 15:45:58  jyh
+ * Optimizing compiler.
+ *
  * Revision 1.7  1998/06/16 16:25:48  jyh
  * Added itt_test.
  *
--- refiner/reflib/ml_term.ml
+++ refiner/reflib/ml_term.ml
@@ -15,7 +15,9 @@ let string_of_term (t : term) =
  * Convert from a string.
  *)
 let term_of_string s =
-   (Marshal.from_string s 0 : term)
+   let t = (Marshal.from_string s 0 : term) in
+      normalize_term t;
+      t
 
 (*
  * Convert to a string.
@@ -27,11 +29,15 @@ let string_of_mterm (t : meta_term) =
  * Convert from a string.
  *)
 let mterm_of_string s =
-   (Marshal.from_string s 0 : meta_term)
-
+   let t = (Marshal.from_string s 0 : meta_term) in
+      normalize_mterm t;
+      t
 
 (*
  * $Log$
+ * Revision 1.2  1998/06/17 15:46:00  jyh
+ * Optimizing compiler.
+ *
  * Revision 1.1  1998/06/17 13:18:31  jyh
  * Added ml_term.
  *
