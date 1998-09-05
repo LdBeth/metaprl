38717506c3ee72526242be094c971ad01fbd8e0c
jyh <jyh@5109f7fa-8300-0410-942d-a7a480986ffa>
1998-09-05 22:31:37 +0000

    Added license headers to each of the files in preparation for the first major release.  The license is GNU public license; if any of you have problems with that, let me know right away.  When you add new code, you should credit yourself as the author.  When you modify code, you should add a "Modified by:" to the header, and possibly a short summary of your changes.
    I tried to get the Author lists as correct as I remember, but there
    are more than 550 files(!) and I may have made some mistakes. Please
    add yourself if I didn't do it right.

--- editor/ml/package_int.mli
+++ editor/ml/package_int.mli
@@ -2,6 +2,34 @@
  * There are three types of packages.  This is an interactive
  * package that contains interactive proofs, theorem statements, etc.
  * A package has a name, and a magic number.
+ *
+ * ----------------------------------------------------------------
+ *
+ * This file is part of Nuprl-Light, a modular, higher order
+ * logical framework that provides a logical programming
+ * environment for OCaml and other languages.
+ *
+ * See the file doc/index.html for information on Nuprl,
+ * OCaml, and more information about this system.
+ *
+ * Copyright (C) 1998 Jason Hickey, Cornell University
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * 
+ * Author: Jason Hickey
+ * jyh@cs.cornell.edu
  *)
 
 include Package_type
--- refiner/term_ds/term_ds.mli
+++ refiner/term_ds/term_ds.mli
@@ -8,6 +8,34 @@
  * and _replace_ the referenced term with the resulting Term term_single.
  * alpha_equal* functions may push down and eliminate all the Substs, not
  * only the top ones.
+ *
+ * ----------------------------------------------------------------
+ *
+ * This file is part of Nuprl-Light, a modular, higher order
+ * logical framework that provides a logical programming
+ * environment for OCaml and other languages.
+ *
+ * See the file doc/index.html for information on Nuprl,
+ * OCaml, and more information about this system.
+ *
+ * Copyright (C) 1998 Alexey Nogin, Cornell University
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * 
+ * Author: Alexey Nogin
+ * jyh@cs.cornell.edu
  *)
 
 open Term_ds_sig
