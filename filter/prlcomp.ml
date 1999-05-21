4207e7a5543c4338d3d46670e0ad1df65cdc369a
jyh <jyh@5109f7fa-8300-0410-942d-a7a480986ffa>
1999-05-21 19:03:44 +0000

    Added MPPATH environment variable (as requested by Mark Bickford) for specifying an include path for prlc.  Directories are separated by :, and -I <dir> command line arguments will append to this path.

--- filter/prlcomp.ml
+++ filter/prlcomp.ml
@@ -26,21 +26,21 @@
  * OCaml, and more information about this system.
  *
  * Copyright (C) 1998 Jason Hickey, Cornell University
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- * 
+ *
  * Author: Jason Hickey
  * jyh@cs.cornell.edu
  *)
@@ -161,6 +161,14 @@ let set_includes () =
       if !verbose_mode > 0 then
          eprintf "%s%t" var eflush;
       Punix.putenv var
+
+(*
+ * Allow include path to be set from the environment.
+ *)
+let _ =
+   try List.iter add_include (String_util.split ':' (Sys.getenv "MPPATH")) with
+      Not_found ->
+         ()
 
 (*
  * Executable names.
