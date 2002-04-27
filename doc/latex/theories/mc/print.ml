06a5ec2d5768257c2b599c47b30a40e685b8fc3f
emre <emre@5109f7fa-8300-0410-942d-a7a480986ffa>
2002-04-27 02:43:27 +0000

    Adding file to specify which modules to print documentation for. (And adding .cvsignore to ignore generated files.)

---
+++ doc/latex/theories/mc/print.ml
@@ -0,0 +1,43 @@
+(*
+ *
+ * ----------------------------------------------------------------
+ *
+ * Copyright (C) 2002 Brian Emre Aydemir, Caltech
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * Author: Brian Emre Aydemir
+ * emre@its.caltech.edu
+ *)
+
+set_tex_file "../../doc/latex/theories/mc/theory.tex";;
+
+print_theory "mp_mc_term_op";;
+print_theory "mp_mc_fir_base";;
+print_theory "mp_mc_fir_ty";;
+print_theory "mp_mc_fir_exp";;
+print_theory "mp_mc_fir_eval";;
+print_theory "mp_mc_deadcode";;
+print_theory "mp_mc_const_elim";;
+print_theory "mp_mc_theory";;
+
+
+(*
+ * -*-
+ * Local Variables:
+ * Caml-master: "compile"
+ * End:
+ * -*-
+ *)
