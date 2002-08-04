71b91c3b52541c333ea56d3b12c049d3aff906cf
emre <emre@5109f7fa-8300-0410-942d-a7a480986ffa>
2002-08-04 19:46:26 +0000

    This is the initial commit of the new FIR theory.  This commit contains the initial set of term declarations, display forms, and documentation.  The next step will be to work toward writing up some typing rules.

---
+++ doc/latex/theories/fir/print.ml
@@ -0,0 +1,40 @@
+(*
+ * Used to print the FIR theory.
+ *
+ * ------------------------------------------------------------------------
+ *
+ * @begin[license]
+ * This file is part of MetaPRL, a modular, higher order
+ * logical framework that provides a logical programming
+ * environment for OCaml and other languages.  Additional
+ * information about the system is available at
+ * http://www.metaprl.org/
+ *
+ * Copyright (C) 2002 Brian Emre Aydemir, Caltech
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * Author: Brian Emre Aydemir
+ * @email{emre@cs.caltech.edu}
+ * @end[license]
+ *)
+
+set_tex_file "../../doc/latex/theories/fir/theory.tex";;
+
+print_theory "mfir_theory";;
+print_theory "mfir_comment";;
+print_theory "mfir_basic";;
+print_theory "mfir_ty";;
+print_theory "mfir_exp";;
