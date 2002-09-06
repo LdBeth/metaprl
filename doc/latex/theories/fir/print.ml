78fc9e7bf599b23f5642b95f9030a71caaf67c25
emre <emre@5109f7fa-8300-0410-942d-a7a480986ffa>
2002-09-06 21:10:07 +0000

    Updating the list of modules to print out.

--- doc/latex/theories/fir/print.ml
+++ doc/latex/theories/fir/print.ml
@@ -34,12 +34,15 @@
 set_tex_file "../../doc/latex/theories/fir/theory.tex";;
 
 print_theory "mfir_theory";;
+print_theory "mfir_option";;
+print_theory "mfir_record";;
 print_theory "mfir_bool";;
 print_theory "mfir_int";;
 print_theory "mfir_list";;
 print_theory "mfir_int_set";;
 print_theory "mfir_ty";;
 print_theory "mfir_exp";;
+(*
 print_theory "mfir_util";;
 print_theory "mfir_sequent";;
 print_theory "mfir_tr_base";;
@@ -47,3 +50,4 @@ print_theory "mfir_tr_types";;
 print_theory "mfir_tr_atom";;
 print_theory "mfir_tr_store";;
 print_theory "mfir_tr_exp";;
+*)
