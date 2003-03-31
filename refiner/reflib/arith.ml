a173c507aa64c13c669fa4a7787fa1cb95748200
yegor <yegor@5109f7fa-8300-0410-942d-a7a480986ffa>
2003-03-31 21:25:12 +0000

    1.GPL header was missing in arith.ml, arith.mli 2.Trying to prove itt_bool/not_assert_elim but don't know how (tried several formulations already) 3.negation in coclusion is now supported by arithT.

--- refiner/reflib/arith.ml
+++ refiner/reflib/arith.ml
@@ -1,3 +1,37 @@
+(*
+ *
+ * Positive cycle search in a graph.
+ * Used by itt_int_arith/arithT
+ *
+ * ----------------------------------------------------------------
+ *
+ * This file is part of MetaPRL, a modular, higher order
+ * logical framework that provides a logical programming
+ * environment for OCaml and other languages.
+ *
+ * See the file doc/index.html for information on Nuprl,
+ * OCaml, and more information about this system.
+ *
+ * Copyright (C) 1998 Jason Hickey, Cornell University
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * Author: Yegor Bryukhov @email{ynb@mail.ru}
+ * Modified by: Aleksey Nogin @email{nogin@cs.cornell.edu}
+ *)
+
 module type HypsSig = sig
     type var
     type cmp = var * var * int
