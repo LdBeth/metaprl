58ce63c5a109557f7969ce43e7529e1c10161116
nogin <nogin@5109f7fa-8300-0410-942d-a7a480986ffa>
2003-04-08 01:14:30 +0000

    Made it more robust.

--- util/xfontsel-pattern.sh
+++ util/xfontsel-pattern.sh
@@ -6,7 +6,7 @@ awk '
 BEGIN { COL=1 }
 (COL<3) { cols[COL]=$0; COL++; next }
 (COL>=3) { print cols[1] "  " cols[2] "  " $0; COL=1; next }
-END { if (COL==2) { print cols[1] }; if (COL==3) { print cols[1] "  " cols[2] }}' | \
-column -t
+END { if (COL==2) { print cols[1] }; if (COL==3) { print cols[1] "  " cols[2] }}' |
+LC_ALL=en_US.UTF-8 column -t
 
 
