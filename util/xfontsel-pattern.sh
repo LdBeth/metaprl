498e1b5004d181de6f725d802bf7bdd1eb0dc6c6
nogin <nogin@5109f7fa-8300-0410-942d-a7a480986ffa>
2003-04-08 01:25:20 +0000

    Furthe font selector script fixes.

--- util/xfontsel-pattern.sh
+++ util/xfontsel-pattern.sh
@@ -5,8 +5,7 @@ sed -e 's|.*::[[:blank:]]*||' -e 's|[[:blank:]]*=[[:blank:]]*|: |' | \
 awk '
 BEGIN { COL=1 }
 (COL<3) { cols[COL]=$0; COL++; next }
-(COL>=3) { print cols[1] "  " cols[2] "  " $0; COL=1; next }
-END { if (COL==2) { print cols[1] }; if (COL==3) { print cols[1] "  " cols[2] }}' |
-LC_ALL=en_US.UTF-8 column -t
+(COL>=3) { printf "%25s %25s %25s\n", cols[1], cols[2], $0; COL=1; next }
+END { if (COL==2) { printf "%25s\n", cols[1] }; if (COL==3) { printf "%25s %25s\n", cols[1], cols[2] }}'
 
 
--- util/xfontsel.sh
+++ util/xfontsel.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+export LANG=en_US.UTF-8 LC_ALL=en_US.UTF-8
 cd "`dirname $0`"
 SAMPLE=`./xfontsel-pattern.sh`
 xfontsel -sampleUCS "$SAMPLE" -pattern '*-iso10646-*'
