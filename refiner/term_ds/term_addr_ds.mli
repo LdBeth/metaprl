72da3facd8997bc50c0482255b785fe43d9759d1
nogin <nogin@5109f7fa-8300-0410-942d-a7a480986ffa>
2004-05-22 04:34:10 +0000

    Oops, forgot the .mli change in the last commit.

--- refiner/term_ds/term_addr_ds.mli
+++ refiner/term_ds/term_addr_ds.mli
@@ -10,21 +10,21 @@
  * OCaml, and more information about this system.
  *
  * Copyright (C) 1998 Alexey Nogin, Cornell University
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- * 
+ *
  * Authors: Aleksey Nogin <nogin@cs.cornell.edu>
  *)
 
@@ -37,11 +37,12 @@ open Term_addr_sig
 open Term_man_sig
 
 type addr =
-   Path of int list
+   Subterm of int
  | ArgAddr
  | HypAddr of int
  | GoalAddr of int
  | Compose of addr * addr
+ | Null
 
 module TermAddr (**)
    (Term : TermDsSig
