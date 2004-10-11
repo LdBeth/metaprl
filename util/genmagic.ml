6787daea8fcfc94d87becdf4fdfa8e5528c1574e
nogin <nogin@5109f7fa-8300-0410-942d-a7a480986ffa>
2004-10-11 10:10:37 +0000

    I've merged the new_parser branch with the trunk. I am putting the result of the merge onto a new branch - "new_parser2" as there seems to be a few issues with it.

---
+++ util/genmagic.ml
@@ -0,0 +1,66 @@
+(*
+ * Generate a magic number for a file.
+ * We do this by taking an MD5 digest,
+ * then converting the first 4 chars to an int.
+ *
+ * The output is in the form of a let-expression.
+ *
+ * ----------------------------------------------------------------
+ *
+ * @begin[license]
+ * Copyright (C) 2004 Mojave Group, Caltech
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * Author: Jason Hickey
+ * @email{jyh@cs.caltech.edu}
+ * @end[license]
+ *)
+open Printf
+
+(*
+ * Compute a magic number for a file.
+ *)
+let genmagic filename =
+   let digest = Digest.file filename in
+   let digest =
+      ((Char.code digest.[0]) lsl 24)
+      lor ((Char.code digest.[1]) lsl 16)
+      lor ((Char.code digest.[2]) lsl 8)
+      lor (Char.code digest.[3])
+   in
+   let name = Filename.basename filename in
+   let name = Filename.chop_extension name in
+      printf "let %s_magic = 0x%08x\n" name digest
+
+(*
+ * Compute magic numbers for all the files.
+ *)
+let main () =
+   Arg.parse [] genmagic "Compute magic numbers for files";
+   flush stdout
+
+let _ =
+   Printexc.catch main ()
+
+(*!
+ * @docoff
+ *
+ * -*-
+ * Local Variables:
+ * Caml-master: "compile"
+ * End:
+ * -*-
+ *)
