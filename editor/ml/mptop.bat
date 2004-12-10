1c73bda018621049a08026125e0ed344eea60c77
yegor <yegor@5109f7fa-8300-0410-942d-a7a480986ffa>
2004-12-10 06:59:54 +0000

    1. I believed that with this commit and bug 361 fixed mldebug.dir and theories.dir will be generated correctly for win32. bat-files have new set-commands, they just have to be uncommented (and old one removed) 2. I also replaced (again) hard-coded paths to SSL library under win32 with variables that can be set in mk/config.local

--- editor/ml/mptop.bat
+++ editor/ml/mptop.bat
@@ -11,6 +11,11 @@ set ENS_ID=$USER
 set ENS_DOMAIN_PORT=7473
 
 rem
+rem Root directory is two directories up
+rem
+set MP_ROOT=../..
+
+rem
 rem Use the local copy of the library
 rem
 set MPLIB=../../lib
@@ -23,11 +28,14 @@ set LC_ALL=en_US.UTF-8
 rem
 rem Search path
 rem
-set INCLUDES=-I . -I ../../support/display -I ../../support/shell -I ../../support/tactics -I ../../theories/sil -I ../../theories/reflect_itt -I ../../theories/tutorial -I ../../theories/tptp -I ../../theories/fol -I ../../theories/czf -I ../../theories/itt -I ../../theories/base -I ../../theories/ocaml_sos -I ../../theories/ocaml_doc -I ../../theories/mc -I ../../theories/fir -I ../../theories/phobos -I ../../theories/experimental/compile -I tests -I $CAMLP4LIB
+set INCLUDES=-I . -I ../../support/display -I ../../support/shell -I ../../support/tactics -I ../../theories/sil -I ../../theories/reflect_itt -I ../../theories/tutorial -I ../../theories/tptp -I ../../theories/fol -I ../../theories/czf -I ../../theories/itt -I ../../theories/base -I ../../theories/ocaml_sos -I ../../theories/ocaml_doc -I ../../theories/mc -I ../../theories/fir -I ../../theories/phobos -I ../../theories/experimental/compile -I ../../theories/cic -I tests -I $CAMLP4LIB
 
 set DEBUGINCLUDES=%INCLUDES% -I ../../mllib -I ../../refiner/refbase -I ../../refiner/refsig -I ../../refiner/term_gen -I ../../refiner/term_std -I ../../refiner/term_ds -I ../../refiner/rewrite -I ../../refiner/refiner -I ../../refiner/reflib -I ../../filter/base -I ../../filter/boot -I ../../filter/filter
+
+rem set /p INCLUDES <theories.dir
+rem set /p DEBUGINCLUDES <mldebug.dir
 
 rem
 rem Start the executable.
 rem
-mp.top %INCLUDES%
+mp.top %INCLUDES% %*
