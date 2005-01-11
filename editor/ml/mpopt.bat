904f356afd9365e255a6f745546f6f9b0cc42629
nogin <nogin@5109f7fa-8300-0410-942d-a7a480986ffa>
2005-01-11 04:56:16 +0000

    - Require OMake 0.9.4 - (bug 118) use the generated include directory list on W32 as well.

--- editor/ml/mpopt.bat
+++ editor/ml/mpopt.bat
@@ -28,11 +28,8 @@ set LC_ALL=en_US.UTF-8
 rem
 rem Search path
 rem
-set INCLUDES=-I . -I ../../support/display -I ../../support/shell -I ../../support/tactics -I ../../theories/sil -I ../../theories/reflect_itt -I ../../theories/tutorial -I ../../theories/tptp -I ../../theories/fol -I ../../theories/czf -I ../../theories/itt -I ../../theories/base -I ../../theories/ocaml_sos -I ../../theories/ocaml_doc -I ../../theories/mc -I ../../theories/fir -I ../../theories/phobos -I ../../theories/experimental/compile -I tests -I $CAMLP4LIB
-rem set /P INCLUDES= <theories.dir
-
-set DEBUGINCLUDES=%INCLUDES% -I ../../mllib -I ../../refiner/refbase -I ../../refiner/refsig -I ../../refiner/term_gen -I ../../refiner/term_std -I ../../refiner/term_ds -I ../../refiner/rewrite -I ../../refiner/refiner -I ../../refiner/reflib -I ../../filter/base -I ../../filter/boot -I ../../filter/filter
-rem set /P DEBUGINCLUDES= <mldebug.dir
+set /P INCLUDES= <theories.dir
+set /P DEBUGINCLUDES= <mldebug.dir
 
 rem
 rem Start the executable.
--- editor/ml/mptop.bat
+++ editor/ml/mptop.bat
@@ -28,12 +28,8 @@ set LC_ALL=en_US.UTF-8
 rem
 rem Search path
 rem
-set INCLUDES=-I . -I ../../support/display -I ../../support/shell -I ../../support/tactics -I ../../theories/sil -I ../../theories/reflect_itt -I ../../theories/tutorial -I ../../theories/tptp -I ../../theories/fol -I ../../theories/czf -I ../../theories/itt -I ../../theories/base -I ../../theories/ocaml_sos -I ../../theories/ocaml_doc -I ../../theories/mc -I ../../theories/fir -I ../../theories/phobos -I ../../theories/experimental/compile -I ../../theories/cic -I tests -I $CAMLP4LIB
-
-set DEBUGINCLUDES=%INCLUDES% -I ../../mllib -I ../../refiner/refbase -I ../../refiner/refsig -I ../../refiner/term_gen -I ../../refiner/term_std -I ../../refiner/term_ds -I ../../refiner/rewrite -I ../../refiner/refiner -I ../../refiner/reflib -I ../../filter/base -I ../../filter/boot -I ../../filter/filter
-
-rem set /p INCLUDES <theories.dir
-rem set /p DEBUGINCLUDES <mldebug.dir
+set /p INCLUDES <theories.dir
+set /p DEBUGINCLUDES <mldebug.dir
 
 rem
 rem Start the executable.
