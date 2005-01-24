bfe1eed8963e36760796a431b7e58b9d91c9667f
nogin <nogin@5109f7fa-8300-0410-942d-a7a480986ffa>
2005-01-24 21:09:02 +0000

    The Refiner_debug module compiles now. It probably does not work yet, but if someone want to try, they are welcome to set "TERMS = both" in mk/config

--- refiner/refiner/refiner.ml
+++ refiner/refiner/refiner.ml
@@ -3,5 +3,9 @@ IFDEF TERMS_ds THEN
 ELSE
    IFDEF TERMS_std THEN
       module Refiner = Refiner_std.Refiner
+   ELSE
+      IFDEF TERMS_both THEN
+         module Refiner = Refiner_debug.MakeRefinerDebug(Refiner_std.Refiner)(Refiner_ds.Refiner)
+      ENDIF
    ENDIF
 ENDIF
