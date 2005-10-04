751511ac38fb334b06eeddbb90e5fefc39e14d0c
jyh <jyh@5109f7fa-8300-0410-942d-a7a480986ffa>
2005-10-04 21:56:58 +0000

    I'm having trouble using untyped ITT with Fsub where I want types. This define Ty -> Term so that I can make some progress.

--- support/display/perv.mli
+++ support/display/perv.mli
@@ -62,7 +62,7 @@ declare typeclass Dform <- Term
 (*
  * Terms that describe types are in the @tt{Ty} typeclass.
  *)
-declare typeclass Ty -> Dform
+declare typeclass Ty -> Term
 
 (*
  * The @tt{Prop} typeclass is for terms that represent propositions.
