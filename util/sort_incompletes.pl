786b47b8cbd65e60d8e279413cc64008e3bc70d9
nogin <nogin@5109f7fa-8300-0410-942d-a7a480986ffa>
2006-02-10 05:32:41 +0000

    Proven much stronger elimination rules for Image and union types.
    Note: this does break a few proofs in Itt_hoas_* theories. I will fix those
    over the weekend (and try to clean up them a bit - now that we are using
    pairwise functionality they can be done _much_ simpler).

---
+++ util/sort_incompletes.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl
+chomp($log=`ls -1 ~mojave/public_html/metaprl/logs/incomplete_proofs-* | tail -1`);
+$cmdline = 'grep \'incomplete proof\' ' . $ARGV[0] . ' | awk \'{print $2}\' | diff ' . $log . ' - | grep \'^>\' | colrm 1 2';
+#print("Executing: $cmdline\n");
+open(INC, "-|", $cmdline);
+while(<INC>) {
+    chomp();
+    s/\///;
+    s/\//./;
+    s/(.)/\U$1\E/;
+    $inc{$_}=1;
+}
+close(INC);
+$cmdline = 'MP_DEBUG=load editor/ml/mp -batch 2>&1 < /dev/null';
+#print("Executing: $cmdline\n");
+open(LOAD, "-|", $cmdline);
+while(<LOAD>) {
+    chomp();
+    if (/^Loading .*\./) {
+	s/^Loading //;
+	if (defined ($inc{$_})) {
+	    s/\./\//;
+	    s/(.)/\/\L$1\E/;
+	    print("New incomplete: ", $_, "\n");
+	}
+    }
+}
+close(LOAD);
